a95c3f13d2009c25d9c2c935ca1355f1992008e6
Stelios Tsinontas <stelisevil@hotmail.com>
2018-08-01 00:14:48 +0100

    Edited test suite

--- strings/test.js
+++ strings/test.js
@@ -8,6 +8,7 @@ import {
 } from '.';
 
 describe('Strings - basics', () => {
+
   describe('amString', () => {
     it('should return true if the argument is a string', () => {
       expect(amString('hello')).toEqual(true);
@@ -20,17 +21,23 @@ describe('Strings - basics', () => {
       expect(amString(undefined)).toEqual(false);
     });
   });
-  describe('howLong', () => {
+
+  xdescribe('howLong', () => {
     it('should return the correct int for the size of the string', () => {
       expect(howLong('hello')).toEqual(5);
+      expect(howLong('')).toEqual(0);
+      expect(howLong('a')).toEqual(1);
+      expect(howLong('hello world')).toEqual(11);
     });
   });
-  describe('join', () => {
+
+  xdescribe('join', () => {
     it('should join the 2 strings together', () => {
       expect(join('hello ' ,'there')).toEqual('hello there');
     });
   });
-  describe('characterExists', () => {
+
+  xdescribe('characterExists', () => {
     it('should return true if the character exists in the string', () => {
       expect(characterExists('hello', 'h')).toEqual(true);
     });
@@ -38,12 +45,14 @@ describe('Strings - basics', () => {
       expect(characterExists('hello', 'x')).toEqual(false);
     });
   });
-  describe('whereIsCharacter', () => {
+
+  xdescribe('whereIsCharacter', () => {
     it('should return an int value for the location of the character in the string', () => {
       expect(whereIsCharacter('hello', 'e')).toEqual(1);
     });
   });
-  describe('removeSpaces', () => {
+
+  xdescribe('removeSpaces', () => {
     it('should remove the spaces from a string', () => {
       expect(removeSpaces('hello there')).toEqual('hellothere')
     });
