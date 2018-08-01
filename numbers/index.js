296bd23aef21a2862ca3d7d42ef2d057fe842e2e
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-08-01 22:42:50 +0100

    Commiting number test

---
+++ numbers/index.js
@@ -0,0 +1,97 @@
+/*
+  Number basics
+
+  In this file you will find functions used to assert your understanding of the
+  basics around numbers
+
+  Each function has a corrisponsing test in the ./test.js file for you to read
+  Don't worry too much about the test suite if it doesn't make sense, that comes
+  in time
+
+  Above each function I have written a small description about what I want that
+  function to do
+*/
+
+
+/*
+  I expect this function to tell me if variable num is a number
+*/
+export const amNumber = (num) => {
+
+}
+
+/*
+  I expect this function to return me the sum of a and b
+*/
+export const add = (a, b) => {
+
+}
+
+/*
+  I expect this function to subtract a and b
+*/
+export const subtract = (a, b) => {
+
+}
+
+/*
+  I expect this function to multiple a and b
+*/
+export const multiply = (a, b) => {
+
+}
+
+/*
+  I expect this function to divide a and b
+  I also expect this function to fail gracefully if a user attempts to
+    divide by 0 (i.e. return false)
+*/
+export const divide = (a, b) => {
+
+}
+
+/*
+  I expect this function round me to the nearest int
+*/
+export const round = (num) => {
+
+}
+
+/*
+  I expect this function to round up to the nearest int
+*/
+export const roundUp = (num) => {
+
+}
+
+/*
+  I expect this function to round down to the nearest int
+*/
+export const roundDown = (num) => {
+
+}
+
+/*
+  I expect this function to convert a string into an integer
+    e.g. "22.23" => 22
+  I expect this function to also fail gracefully if the argument cant be
+  converted to a number
+*/
+export const convertToInt = (str) => {
+
+}
+
+/*
+  I expect this function to return true if a number is odd, false if it is even
+*/
+export const isOdd = (num) => {
+
+}
+
+/*
+  I expect this function to take the numnber and round it to the decmials
+  provided by the decimals argument
+*/
+export const variableDecimalPlace = (num, decimals) => {
+
+}
---
+++ numbers/test.js
@@ -0,0 +1,108 @@
+import {
+  amNumber,
+  add,
+  subtract,
+  multiply,
+  divide,
+  isOdd,
+  round,
+  roundUp,
+  roundDown,
+  convertToNumber,
+  convertToInt,
+  variableDecimalPlace
+} from '.';
+
+describe('Number test', () => {
+  describe('amNumber', () => {
+    it('returns true if the value is a number', () => {
+      expect(amNumber(0)).toEqual(true);
+      expect(amNumber(17.3)).toEqual(true);
+      expect(amNumber(-11)).toEqual(true);
+    });
+    it('returns false if the value isnt a number', () => {
+      expect(amNumber(false)).toEqual(false);
+      expect(amNumber([])).toEqual(false);
+      expect(amNumber(null)).toEqual(false);
+      expect(amNumber()).toEqual(false);
+    });
+  });
+  describe('add', () => {
+    it('adds 2 numbers together', () => {
+      expect(add(1, 2)).toEqual(3);
+      expect(add(3.4, 1.9)).toEqual(5.3)
+    });
+  });
+  describe('subtract', () => {
+    it('subtracts the 2 numbers togetger', () => {
+      expect(subtract(2, 3)).toEqual(-1);
+      expect(subtract(2, -3)).toEqual(5);
+      expect(subtract(-11, -23)).toEqual(12);
+    });
+  });
+  describe('multiply', () => {
+    it('multiplies the 2 numbers together', () => {
+      expect(multiply(1, 2)).toEqual(2);
+      expect(multiply(7, -3)).toEqual(-21);
+      expect(multiply(0, 1)).toEqual(0);
+    });
+  });
+  describe('divide', () => {
+    it('divides the 2 numbers together', () => {
+      expect(divide(1, 2)).toEqual(0.5);
+      expect(divide(0, 5)).toEqual(0);
+    });
+    it('fails gracefully if trying to divide by 0', () => {
+      expect(divide(5, 0)).toEqual(false);
+    });
+  });
+  describe('round', () => {
+    it('rounds to the nearest int', () => {
+      expect(round(7.2)).toEqual(7);
+      expect(round(-1.2)).toEqual(-1);
+      expect(round(0.51)).toEqual(1);
+    });
+  });
+  describe('roundUp', () => {
+    it('rounds up to the next int', () => {
+      expect(roundUp(1.1)).toEqual(2);
+      expect(roundUp(-11.9)).toEqual(-11);
+      expect(roundUp(0)).toEqual(0);
+    });
+  })
+  describe('roundDown', () => {
+    it('rounds down to the next int', () => {
+      expect(roundDown(1.1)).toEqual(1);
+      expect(roundDown(-11.9)).toEqual(-12);
+      expect(roundDown(0)).toEqual(0);
+    });
+  })
+  describe('convertToInt', () => {
+    it('converts to an integer', () => {
+      expect(convertToInt('123.2')).toEqual(123);
+      expect(convertToInt('-11.2')).toEqual(-11);
+    });
+    it('fails gracefully if cannot be converted to an int', () => {
+      expect(convertToInt('hello')).toEqual(false);
+      expect(convertToInt({})).toEqual(false);
+      expect(convertToInt([])).toEqual(false);
+      expect(convertToInt()).toEqual(false);
+    });
+  });
+  describe('isOdd', () => {
+    it('returns true if the number is odd', () => {
+      expect(isOdd(1)).toEqual(true);
+      expect(isOdd(11)).toEqual(true);
+    });
+    it('returns false if the number is even', () => {
+      expect(isOdd(2)).toEqual(false);
+      expect(isOdd(10)).toEqual(false)
+    });
+  });
+  describe('variableDecimalPlace', () => {
+    it('converst to the correct number of decimal places', () => {
+      expect(variableDecimalPlace('93.48', 1)).toEqual(93.5);
+      expect(variableDecimalPlace('103.93302', 4)).toEqual(103.9330);
+    });
+  });
+});
