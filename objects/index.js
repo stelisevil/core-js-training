b6dd93e22fdb6c4135b4094e49c61e2176f97746
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-08-02 21:34:21 +0100

    Object test

---
+++ objects/index.js
@@ -0,0 +1,80 @@
+/*
+  Object basics
+
+  In this file you will find functions used to assert your understanding of the
+  basics around objects
+
+  Each function has a corrisponsing test in the ./test.js file for you to read
+  Don't worry too much about the test suite if it doesn't make sense, that comes
+  in time
+
+  Above each function I have written a small description about what I want that
+  function to do
+
+  In case you need a refresher, here is what an object looks like, note the {}
+  bracket notation, the : after a key (which is camelCased) and the comma after
+  a value to note the end of that key:value pair
+
+    const obj = {
+      key: 'value',
+      anotherKey: 'another-value',
+      age: 12,
+      property: true
+    };
+
+  Objects are perhaps the one data type you will deal with the most once you
+  get into React so its important you understand them well
+
+  You may see the phraes key and property used interchangeably when discussing
+  objects, they both mean the same thing
+
+  Anyway... onto the test
+*/
+
+/*
+  I expect this function to return true if obj is an object
+    ...be careful with this one... ;)
+*/
+export const isObject = (obj) => {
+
+}
+
+/*
+  I expect this function to return the firstName property of the object
+  If the property firstName doesn't exist on the object, return false
+*/
+export const getFirstName = (obj) => {
+
+}
+
+/*
+  I expect this function to return whichever property is passed in from the
+    object. Fail gracefully if not found
+*/
+export const getKeyFromObject = (obj, key) => {
+
+}
+
+/*
+  I expect this function to return me an array of the objects keys
+*/
+export const getObjectKeys = (obj) => {
+
+}
+
+/*
+  I expect this function to return me an array of the objects values
+*/
+export const getObjectValues = (obj) => {
+
+}
+
+/*
+  I expect this function to join the 2 objects together, returning a single
+    object which contains properties from both.
+  If objB has a property which is the same as a property of A, then B should
+    take priority as you're copying B into A
+*/
+export const joinObjects = (objA, objB) => {
+
+}
---
+++ objects/test.js
@@ -0,0 +1,75 @@
+import {
+  isObject,
+  getFirstName,
+  getKeyFromObject,
+  getObjectKeys,
+  getObjectValues,
+  joinObjects
+} from '.';
+
+const user = {
+  firstName: 'Matthew',
+  lastName: 'Tsinontas',
+  email: 'matthew@gmail.com'
+};
+
+const address = {
+  address1: 'Apartment 2003',
+  address2: '100 West Street'
+};
+
+describe('Object test', () => {
+  describe('isObject', () => {
+    it('returns true if an object', () => {
+      expect(isObject({})).toEqual(true);
+    });
+    it('returns false if not an object', () => {
+      expect(isObject()).toEqual(false);
+      expect(isObject('hello')).toEqual(false);
+      expect(isObject(true)).toEqual(false);
+      expect(isObject(0)).toEqual(false);
+      expect(isObject([])).toEqual(false);
+      expect(isObject(null)).toEqual(false);
+    });
+  });
+  describe('getFirstName', () => {
+    it('returns the firstName property if it exists', () => {
+      expect(getFirstName(user)).toEqual('Matthew');
+    });
+    it('returns false if it doesnt', () => {
+      expect(getFirstName({})).toEqual(false);
+    });
+  });
+  describe('getKeyFromObject', () => {
+    it('returns the key for the object', () => {
+      expect(getKeyFromObject(user, 'email')).toEqual('matthew@gmail.com')
+    });
+    it('returns false if it doesnt exist', () => {
+      expect(getKeyFromObject(user, 'random-key')).toEqual(false);
+    })
+  });
+  describe('getObjectKeys', () => {
+    it('returns an array of keys', () => {
+      expect(getObjectKeys(user)).toEqual(['firstName', 'lastName', 'email']);
+    });
+  });
+  describe('getObjectValues', () => {
+    it('returns an array of values', () => {
+      expect(getObjectValues(user)).toEqual(['Matthew', 'Tsinontas', 'matthew@gmail.com']);
+    });
+  });
+  describe('joinObjects', () => {
+    it('joins 2 objects together', () => {
+      expect(joinObjects(user, address)).toEqual({
+        firstName: 'Matthew',
+        lastName: 'Tsinontas',
+        email: 'matthew@gmail.com',
+        address1: 'Apartment 2003',
+        address2: '100 West Street'
+      })
+    });
+    it('overwrites the properties of objA with objB', () => {
+      expect(joinObjects({ foo: 'bar' }, { foo: 'bat' })).toEqual({ foo: 'bat' });
+    });
+  });
+});
